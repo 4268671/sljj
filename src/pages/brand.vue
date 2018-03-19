178caeb4d3f98baf4039c3d55fb68d81f7a6070b
Yann : ) <4268671@qq.com>
2018-03-19 12:35:57 +0800

    1111

--- src/pages/brand.vue
+++ src/pages/brand.vue
@@ -3,7 +3,6 @@
         <div class="w3-margin-bottom">
             <img :src="`${URLPREFIX}${channelthumb}`" alt="" class="w3-block wow fadeInUp w3-card">
         </div>
-        {{brandlist}}
         <div style="max-width:1500px;margin:auto">
             <div class="w3-padding-24 wow fadeInUp">
                 <h2 class="w3-center w3-panel">舒览品牌介绍
@@ -21,30 +20,19 @@
                     </h3>
                     <div class="w3-container w3-text-gray">
                         <div class="w3-border-bottom w3-padding-16 w3-border-red">
-                            <p>我们做的不只是床垫，更是一流的设计与内在。 </p>
-                            <p>我们带来的不仅是好眠，更是您和家人幸福的源泉。 </p>
-                            <p>您并不仅仅是购买了一张床垫，而是把生活态度带回了家。</p>
+                            <p v-for="(item,key) in item.subtitle.split('\n')" :key="key">
+                                {{item}}
+                            </p>
                         </div>
-                        <p>
-                            我们不仅是舒览，<br>更是将伴您一生的美好回忆。
-                        </p>
+                        <div>
+                            <p v-for="(item,key) in item.description.split('\n')" :key="key">
+                                {{item}}
+                            </p>
+                        </div>
                     </div>
                 </div>
+                <div class="w3-clear"></div>
             </div>
-            <div class="w3-clear"></div>
-            <div class="w3-padding-32">
-                <div class="w3-half w3-right w3-padding wow fadeInRight">
-                    <img :src="`${URLPREFIX}/static/brand04.jpg`" alt="" class="w3-block w3-card">
-
-                </div>
-                <div class="w3-rest w3-padding wow fadeInRight">
-                    <h1>INSPECTION REPORT</h1>
-                    <h3 class="w3-leftbar w3-padding w3-border-red">舒览对您无所保留 /
-                        <span class="w3-text-red w3-medium">质检报告</span>
-                    </h3>
-                </div>
-            </div>
-            <div class="w3-clear"></div>
         </div>
     </div>
 </template>
